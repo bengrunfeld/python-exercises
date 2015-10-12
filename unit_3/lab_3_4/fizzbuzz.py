f2eb637d3fc2ad01bbd40abe65a2303f2726798f
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-10-12 00:55:27 -0600

    Add lab 3.4 - fizzbuzz

---
+++ unit_3/lab_3_4/fizzbuzz.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+for fb in range(100):
+    if fb % 3 == 0 and fb % 5 == 0:
+        print "fizzbuzz"
+        continue
+    elif fb % 3 == 0:
+        print "fizz"
+        continue
+    elif fb % 5 == 0:
+        print "buzz"
+        continue
+    print fb
+
