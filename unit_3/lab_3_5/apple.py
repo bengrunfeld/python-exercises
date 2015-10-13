c30173fd0ae2374d92dc310cd35bb2072ffbfa17
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-10-13 02:19:59 -0600

    Add labs in units 3 and 4

---
+++ unit_3/lab_3_5/apple.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+def apple_pi(a, b):
+    """Return sum of the inputs multiplied by Pi"""
+
+    c = a + b
+    return c * 3.141592
+
+result = apple_pi(5, 10)
+print result
---
+++ unit_4/lab_4_2/tuple_iteration.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+
+lottery_tuple = (32, 81, 74, 16, 25, 90, 53)
+
+for index, lottery_number in enumerate(lottery_tuple):
+    print "Lottery number {}: is {}".format(index, lottery_number)
