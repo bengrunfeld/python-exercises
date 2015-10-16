7150a02a7cdb4815e7050bf8d25b0ee2c413c207
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-10-15 23:47:11 -0700

    update notes

---
+++ unit_4/lab_4_4/example.py
@@ -0,0 +1,13 @@
+class MyClass:
+    """A simple example class""" 
+    i = 12345
+    def f(self):
+        print 'hello world'
+        return 'hello world'
+
+
+x = MyClass()
+
+x.f()
+
+print __name__
--- unit_6/lab_6_1/shoddy_work.py
+++ unit_6/lab_6_1/shoddy_work.py
@@ -4,9 +4,13 @@ menu = {}
 blends = ('lovo', 'anduino', 'especiale')
 coffee_types = [x.upper() for x in blends if len(x) > 4]
 
-for i, coffee in enumerate(coffee_types):
-    menu[coffee] = "{}:{} is a medium roast".format(i, coffee)    
+def write_menu(coffee_list):
+    for i, coffee in enumerate(coffee_list):
+        menu[coffee] = "{}.{} is a medium roast".format(i, coffee)    
+write_menu(coffee_types)
 
+print 'DICTIONARY OF COFFEE'
+print '---------------'
 print menu
 
 
